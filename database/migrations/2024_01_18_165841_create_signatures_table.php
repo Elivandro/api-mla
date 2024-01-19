eb73d6e3ac1bf0c469517a60246a21d224b73076
Elivandro <Elivandro@hotmail.com.br>
2024-01-19 07:10:43 -0400

    migrations and models created

--- database/migrations/2024_01_18_165841_create_signatures_table.php
+++ database/migrations/2024_01_18_165841_create_signatures_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('signatures', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('client_id')->constrained();
+            $table->foreignId('plan_id')->constrained();
+            $table->char('status',1);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('signatures');
     }
 };
