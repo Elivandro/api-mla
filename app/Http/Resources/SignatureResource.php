6be1629b686b1b56b6494290b0f3ad41cc8d50f2
Elivandro <Elivandro@hotmail.com.br>
2024-01-20 18:09:50 -0400

    signature resource for clients created

---
+++ app/Http/Resources/SignatureResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SignatureResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'status' => $this->status,
+            'created_at' => $this->created_at
+        ];
+    }
+}
