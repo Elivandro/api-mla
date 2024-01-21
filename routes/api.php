951e82761b158c65323db4277d57481f911912ed
Elivandro <Elivandro@hotmail.com.br>
2024-01-21 12:01:05 -0400

    login/logout and group clients routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
 use Illuminate\Http\Request;
@@ -15,9 +16,17 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', [AuthController::class, 'login'])->name('login');
+
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::apiResource('clients', ClientController::class);
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
 });
-
-
-Route::apiResource('clients', ClientController::class);
